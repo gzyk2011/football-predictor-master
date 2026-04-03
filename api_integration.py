9a124b4c9f9de4fd5626fd9445961be55796b091
LIN YONGCAI <522729335@qq.com>
2026-04-03 23:44:28 +0800

    Update api_integration.py

--- api_integration.py
+++ api_integration.py
@@ -227,6 +227,7 @@ LEAGUES = {
     "England League One": {"api_football_id": 41, "football_data_code": None},
     "England League Two": {"api_football_id": 42, "football_data_code": None},
     "Wales  Premier League": {"api_football_id": 110, "football_data_code": None},
+    "Switzerland 1. Liga Promotion": {"api_football_id": 510, "football_data_code": None},
     "Poland Ekstraklasa": {"api_football_id": 106, "football_data_code": None},
     "England Championship": {"api_football_id": 40, "football_data_code": None},
     "Netherlands Eerste Divisie": {"api_football_id": 89, "football_data_code": None},
